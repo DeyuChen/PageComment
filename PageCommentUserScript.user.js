df2a4bb435a185d65639a235e75c25d0e6da48e7
teyuc <the.deyu.chen@gmail.com>
2021-07-24 22:43:40 -0700

    fix github URL

--- PageCommentUserScript.user.js
+++ PageCommentUserScript.user.js
@@ -2,8 +2,8 @@
 // @name       Sidebar for Page Comments
 // @version    0.2
 // @description  Sidebar for Page Comments
-// @downloadURL https://github.com/DeyuChen/PageComment/blob/main/PageCommentUserScript.user.js
-// @updateURL https://github.com/DeyuChen/PageComment/blob/main/PageCommentUserScript.user.js
+// @downloadURL https://github.com/DeyuChen/PageComment/raw/main/PageCommentUserScript.user.js
+// @updateURL https://github.com/DeyuChen/PageComment/raw/main/PageCommentUserScript.user.js
 // @match *://*/*
 // @require http://code.jquery.com/jquery-latest.js
 // @grant    GM_addStyle
